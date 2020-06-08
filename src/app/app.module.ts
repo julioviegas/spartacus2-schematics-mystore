0717b33c4a57aa2911c5cb73a7755fb1c285cd06
Julio Viegas <julio.viegas@hybris.com>
2020-06-08 17:11:39 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.0'
+      }
+    }),
+    BrowserTransferStateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
