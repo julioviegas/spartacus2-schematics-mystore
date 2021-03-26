d49af7d04bec992ef6ac7e00b5e58ef9216eb2e7
julioviegas <julioviegas@julioviegas.com>
2021-03-26 11:52:51 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,36 @@
-import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  BrowserTransferStateModule
+} from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { translations, translationChunksConfig } from '@spartacus/assets';
-import { B2cStorefrontModule } from '@spartacus/storefront';
+import { AppComponent } from "./app.component";
+import { translations, translationChunksConfig } from "@spartacus/assets";
+import { B2cStorefrontModule } from "@spartacus/storefront";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     B2cStorefrontModule.withConfig({
       backend: {
         occ: {
-          baseUrl: 'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com/',
-          prefix: '/rest/v2/'
+          baseUrl: "https://spartacus-demo.eastus.cloudapp.azure.com/",
+          prefix: "/rest/v2/"
         }
       },
       context: {
-        currency: ['USD'],
-        language: ['en'],
-        baseSite: ['electronics-spa']
+        currency: ["USD"],
+        language: ["en"],
+        baseSite: ["electronics-spa"]
       },
       i18n: {
         resources: translations,
         chunks: translationChunksConfig,
-        fallbackLang: 'en'
+        fallbackLang: "en"
       },
       features: {
-        level: '2.0'
+        level: "2.0"
       }
     }),
     BrowserTransferStateModule
@@ -37,4 +38,4 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
